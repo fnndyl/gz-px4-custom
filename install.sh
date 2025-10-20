f376c2b796ac0f4c6f88fbfd71b1cb3db34d57dc
fnndyl <dylanfanner@gmail.com>
2025-10-20 11:36:20 +0200

    Install process semi working. Need to soruce gz_plugins still, but that can be put into .bashrc or .zshrc

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ -d "$HOME/.simulation-gazebo" ]; then
-	echo "~/.simulation-gazebo directory exists, installing custom content..."
+if [ -d "$HOME/PX4-Autopilot" ]; then
+	echo
+	echo "~/PX4-Autopilot directory exists, installing custom content..."
 	echo
 
-	cp "$SCRIPT_DIR/custom_worlds/"* "$HOME/.simulation-gazebo/worlds"
-	cp -r "$SCRIPT_DIR/custom_models/"* "$HOME/.simulation-gazebo/models"
+	cp "$SCRIPT_DIR/custom_worlds/"* "$HOME/PX4-Autopilot/Tools/simulation/gz/worlds"
+	cp -r "$SCRIPT_DIR/custom_models/"* "$HOME/PX4-Autopilot/Tools/simulation/gz/models"
 
 	# Add gz plugins to search directory
-	#export GZ_SIM_SYSTEM_PLUGIN_PATH=$GZ_SIM_SYSTEM_PLUGIN_PATH:"$SCRIPT_DIR/gz_plugins/barge_controller/build/"
 	export GZ_SIM_SYSTEM_PLUGIN_PATH=$(find "$SCRIPT_DIR/gz_plugins" -type d -name build | paste -sd:)
 
 else
-	echo "No ~/.simulation-gazebo found. Install using python file PX4/Tools/simulation/gz/simulation-gazebo"
+	echo "No ~/PX4-Autopilot found. Install in home directory, or edit install.sh to target correct install directory."
 fi
